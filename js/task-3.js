88553e5a4730b56184372bed66bd2d03d9753e1a
sauergru <sauergru@gmail.com>
2024-03-01 18:52:14 +0100

    finished

---
+++ js/task-3.js
@@ -0,0 +1,28 @@
+class StringBuilder {
+    #value;
+    constructor(initialValue) {
+        this.#value = [initialValue];
+    }
+    getValue() {
+        return this.#value.join('');
+    }
+    padEnd(str) {
+        this.#value.push(str);
+    }
+    padStart(str) {
+        this.#value.unshift(str);
+    }
+    padBoth(str) {
+        this.padStart(str);
+        this.padEnd(str);
+    }
+}
+
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
